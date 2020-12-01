12406989384730a9a32f859e8a4e34a7e274ccb3
Ilia Partuk <ilia@wearebond.com>
2020-11-30 23:41:12 -0500

    Bugfix: install, not update for neovim Plug

--- new-computer-setup.sh
+++ new-computer-setup.sh
@@ -47,7 +47,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
-nvim -c ":PlugUpdate | qa\!"
+nvim -c ":PlugInstall | qa\!"
 
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.1/install.sh | bash ;
