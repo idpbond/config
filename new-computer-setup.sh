7615db3ca14454c2a500a36d90b0e3b9e371bf8e
Ilia Partuk <ilia@wearebond.com>
2021-01-20 23:02:52 -0800

    * Remove dupes * Fix wget outputs

--- new-computer-setup.sh
+++ new-computer-setup.sh
@@ -3,7 +3,7 @@
 $(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh);
 $(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh);
 
-sed -i '' 's/^plugins=(.*/plugins=(git fzf dotenv docker docker-compose)/' ~/.zshrc
+sed -i'' 's/^plugins=(.*/plugins=(git fzf dotenv docker docker-compose)/' ~/.zshrc
 
 brew install tmux neovim wget fzf node git gpg the_silver_searcher redis awscli docker hub tig htop pyenv;
 brew install --cask keepassxc google-backup-and-sync postgres bettertouchtool pg-commander;
@@ -18,8 +18,10 @@ echo "Remember to:
 
 $(brew --prefix)/opt/fzf/install --all;
 
-wget https://raw.githubusercontent.com/idpbond/config/master/.tmux.conf > ~/.tmux.conf;
-wget https://raw.githubusercontent.com/idpbond/config/master/nvim > ~/.config/;
+mkdir -p ~/.config;
+git clone https://github.com/idpbond/config /tmp/config;
+wget https://raw.githubusercontent.com/idpbond/config/master/.tmux.conf -O ~/.tmux.conf;
+cp -r /tmp/config/nvim -O ~/.config/;
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
 
@@ -39,24 +41,11 @@ curl -sSL https://get.rvm.io | bash -s stable --ruby ;
 # TODO BTT import if possible
 # wget https://raw.githubusercontent.com/idpbond/config/master/bettertouchtools-Default.json
 
-$(brew --prefix)/opt/fzf/install --all;
-wget https://raw.githubusercontent.com/idpbond/config/master/.tmux.conf > ~/.tmux.conf;
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
-
-# NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.1/install.sh | bash ;
-
 # Neovim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
-wget https://raw.githubusercontent.com/idpbond/config/master/nvim > ~/.config/;
 nvim -c ":PlugInstall | qa\!"
-
-# RVM
-curl -sSL https://rvm.io/mpapis.asc | gpg --import - ;
-curl -sSL https://rvm.io/pkuczynski.asc | gpg --import - ;
-curl -sSL https://get.rvm.io | bash -s stable --ruby ;
 
 cat ./zsh.partial >> ~/.zshrc
 
