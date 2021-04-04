0d20c3e544b4389ff115d7ac56f558c9a868437a
Ilia Partuk <ilia@wearebond.com>
2021-04-03 23:28:31 -0700

    Add font source for DejaVu Sans Mono Code

--- new-computer-setup.sh
+++ new-computer-setup.sh
@@ -29,6 +29,9 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
+# Fonts (TODO: Automate)
+# https://github.com/mdschweda/dejavusansmonocode
+
 # NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.1/install.sh | bash ;
 echo 'export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
