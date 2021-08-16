482a3e544675c57db3b51afa52fb42467f6b683c
Ilia Partuk <ilia@wearebond.com>
2021-08-15 23:06:18 -0700

    Fix async neovim plugin tasks exiting before actually running

--- new-computer-setup.sh
+++ new-computer-setup.sh
@@ -62,8 +62,8 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm;
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim';
 
-nvim -c ":PlugInstall | qa\!"
-nvim -c ":CocInstall coc-prettier coc-neosnippet coc-git coc-eslint coc-tsserver coc-solargraph coc-json coc-angular coc-sh coc-deno | qa\!"
+nvim +PlugInstall +qall && \
+nvim +'CocInstall -sync coc-prettier coc-neosnippet coc-git coc-eslint coc-tsserver coc-solargraph coc-json coc-angular coc-sh coc-deno' +qall ;
 
 # Fonts (TODO: Automate)
 # TODO: https://github.com/mdschweda/dejavusansmonocode
